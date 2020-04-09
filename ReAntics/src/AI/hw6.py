86081f250d2e0f1690dcccb06a962a62f1886288
Harry Thoma <thoma20@up.edu>
2020-04-09 13:33:43 -0700

    added reward function

--- ReAntics/src/AI/hw6.py
+++ ReAntics/src/AI/hw6.py
@@ -1,3 +1,4 @@
+import math
 import random
 import sys
 sys.path.append("..")  #so other modules can be found in parent dir
@@ -104,6 +105,99 @@ class AIPlayer(Player):
             selectedMove = moves[random.randint(0,len(moves) - 1)];
             
         return selectedMove
+
+    def reward(self, currentState):
+        myState = currentState.fastclone()
+        me = myState.whoseTurn
+        enemy = abs(me - 1)
+        myInv = getCurrPlayerInventory(myState)
+        myFood = myInv.foodCount
+        enemyInv = getEnemyInv(self, myState)
+        tunnels = getConstrList(myState, types=(TUNNEL,))
+        myTunnel = tunnels[1] if (tunnels[0].coords[1] > 5) else tunnels[0]
+        enemyTunnel = tunnels[0] if (myTunnel is tunnels[1]) else tunnels[1]
+        hills = getConstrList(myState, types=(ANTHILL,))
+        myHill = hills[1] if (hills[0].coords[1] > 5) else hills[0]
+        enemyHill = hills[1] if (myHill is hills[0]) else hills[0]
+        enemyQueen = enemyInv.getQueen()
+
+        foods = getConstrList(myState, None, (FOOD,))
+
+        myWorkers = getAntList(myState, me, (WORKER,))
+        myOffense = getAntList(myState, me, (SOLDIER,))
+        enemyWorkers = getAntList(myState, enemy, (WORKER,))
+
+        # "steps" val that will be returned
+        occupyWin = 0
+
+        # keeps one offensive ant spawned
+        # at all times
+        if len(myOffense) < 1:
+            occupyWin += 20
+        elif len(myOffense) <= 2:
+            occupyWin += 30
+
+        # encourage more food gathering
+        if myFood < 1:
+            occupyWin += 20
+
+        # never want 0 workers
+        if len(myWorkers) < 1:
+            occupyWin += 100
+        if len(myWorkers) > 1:
+            occupyWin += 100
+
+        # want to kill enemy queen
+        if enemyQueen == None:
+            occupyWin -= 1000
+
+        # calculation for soldier going
+        # to kill enemyworker and after
+        # going to sit on enemy anthill
+        dist = 100
+        for ant in myOffense:
+            if len(enemyWorkers) == 0:
+                if not enemyQueen == None:
+                    dist = approxDist(ant.coords, enemyHill.coords)
+            else:
+                dist = approxDist(ant.coords, enemyWorkers[0].coords) + 10
+                if len(enemyWorkers) > 1:
+                dist += 10
+
+        occupyWin += (dist) + (enemyHill.captureHealth)
+
+        # Gather food
+        foodWin = occupyWin
+        if not len(myOffense) > 0:
+            foodNeeded = 11 - myFood
+            for w in myWorkers:
+                distanceToTunnel = approxDist(w.coords, myTunnel.coords)
+                distanceToHill = approxDist(w.coords, myHill.coords)
+                distanceToFood = 9999
+                for food in foods:
+                    if approxDist(w.coords, food.coords) < distanceToFood:
+                        distanceToFood = approxDist(w.coords, food.coords)
+                if w.carrying: # if carrying go to hill/tunnel
+                    foodWin += min(distanceToHill, distanceToTunnel) - 9.5
+                    if w.coords == myHill.coords or w.coords == myTunnel.coords:
+                        foodWin += 1.5
+                    if not len(myOffense) == 0:
+                        foodWin -= 1
+                else: # if not carrying go to food
+                    if w.coords == foods[0].coords or w.coords == foods[1].coords:
+                        foodWin += 1.2
+                        break
+                    foodWin += distanceToFood/3 - 1.5
+                    if not len(myOffense) == 0:
+                        foodWin -= 1
+            occupyWin += foodWin * (foodNeeded)
+
+        # encourage queen killing or sitting on tunnel
+        if not enemyQueen == None:
+            if enemyQueen.coords == enemyHill.coords:
+                occupyWin += 20
+
+        return 1-math.exp(-0.001*(occupyWin))
     
     ##
     #getAttack
