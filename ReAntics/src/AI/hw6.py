6200cbf876f116010ad7b543145e030c8a262efa
Harry Thoma <thoma20@up.edu>
2020-04-15 21:05:53 -0700

    q learning

--- ReAntics/src/AI/hw6.py
+++ ReAntics/src/AI/hw6.py
@@ -13,7 +13,7 @@ import random
 import pickle
 
 
-ALPHA = 1
+ALPHA = .5
 GAMMA = 0.9
 
 
@@ -42,13 +42,14 @@ class AIPlayer(Player):
         self.use_saved_weights = True
 
         if self.use_saved_weights:
-            self.state_utility = pickle.load(open("../dict_dump.txt", "rb"))
+            self.state_action_utility = pickle.load(open("../dict_dump.txt", "rb"))
         else:
-            self.state_utility = {}
+            self.state_action_utility = {}
 
         self.explore_probability = 0
 
         self.previous_state = None
+        self.previous_move = None
         self.move_count = 0
 
         self.current_game_states = []
@@ -64,10 +65,11 @@ class AIPlayer(Player):
         return False
 
 
-    def get_utility(self, state):
-        if stateCategory(state) not in self.state_utility:
-            self.state_utility[stateCategory(state)] = self.get_reward(state)
-        return self.state_utility[stateCategory(state)]
+    def get_utility(self, state_action):
+        state, action = state_action
+        if stateCategory(state) not in self.state_action_utility:
+            self.state_action_utility[stateCategory(state)] = self.get_reward(state)
+        return self.state_action_utility[stateCategory(state)]
 
     def get_reward(self, state):
         getWin_val = getWinner(state)
@@ -77,29 +79,30 @@ class AIPlayer(Player):
             return -1
 
         if getWin_val == None:
-            food_reward = getCurrPlayerInventory(state).foodCount * .005
+            food_reward = getCurrPlayerInventory(state).foodCount * .01
             return -0.01 + food_reward
 
-    def set_utility(self, state, value):
-        self.state_utility[stateCategory(state)] = value
+    def set_utility(self, state_action, value):
+        state, action = state_action
+        self.state_action_utility[(stateCategory(state), action)] = value
 
     #update the utility of the given "current_state", given the next_state
     #if there is a winner this state, no current state is needed
-    def update_utility(self, current_state, next_state, won=None):
+    def update_utility(self, current_state_action, next_state_action, won=None):
 
-        if not current_state == None:
+        if not current_state_action == None:
 
             if won is None:
-                currUtility = self.get_utility(next_state)
+                next_utility = self.get_utility(next_state_action)
             else:
-                currUtility = 1 if won else -1
+                next_utility = 1 if won else -1
 
-            prevUtility = self.get_utility(current_state)
-            prevReward = self.get_reward(current_state)
+            curr_utility = self.get_utility(current_state_action)
+            curr_reward = self.get_reward(current_state_action[0])
             
-            updated_utility = prevUtility + ALPHA*(prevReward + GAMMA*currUtility - prevUtility)
+            updated_utility = curr_utility + ALPHA*(curr_reward + GAMMA*next_utility - curr_utility)
             
-            self.set_utility(current_state, updated_utility)
+            self.set_utility(current_state_action, updated_utility)
 
     #udpate the utility of all states seen this game
     def update_game_utilities(self, hasWon):
@@ -183,7 +186,6 @@ class AIPlayer(Player):
             return None
         self.move_count += 1    
 
-        self.current_game_states.append(currentState)
         #self.update_utility(self.previous_state, currentState)
         #self.previous_state = currentState
 
@@ -194,104 +196,16 @@ class AIPlayer(Player):
         for move in moves:
             move_state_list.append((getNextState(currentState, move), move))
 
+
         if self.if_exploring():
-            return random.choice(moves)
+            move_to_make = random.choice(moves)
         else:
-            return max(move_state_list, key = lambda item: self.get_utility(item[0]))[1]
+            #select the move that takes you to the state with the highest utility
+            move_to_make = max(move_state_list, key = self.get_utility)[1]
 
-
-    def reward(self, currentState):
-        myState = currentState.fastclone()
-        me = myState.whoseTurn
-        enemy = abs(me - 1)
-        myInv = getCurrPlayerInventory(myState)
-        myFood = myInv.foodCount
-        enemyInv = getEnemyInv(self, myState)
-        tunnels = getConstrList(myState, types=(TUNNEL,))
-        myTunnel = tunnels[1] if (tunnels[0].coords[1] > 5) else tunnels[0]
-        enemyTunnel = tunnels[0] if (myTunnel is tunnels[1]) else tunnels[1]
-        hills = getConstrList(myState, types=(ANTHILL,))
-        myHill = hills[1] if (hills[0].coords[1] > 5) else hills[0]
-        enemyHill = hills[1] if (myHill is hills[0]) else hills[0]
-        enemyQueen = enemyInv.getQueen()
-
-        foods = getConstrList(myState, None, (FOOD,))
-
-        myWorkers = getAntList(myState, me, (WORKER,))
-        myOffense = getAntList(myState, me, (SOLDIER,))
-        enemyWorkers = getAntList(myState, enemy, (WORKER,))
-
-        # "steps" val that will be returned
-        occupyWin = 0
-
-        # keeps one offensive ant spawned
-        # at all times
-        if len(myOffense) < 1:
-            occupyWin += 20
-        elif len(myOffense) <= 2:
-            occupyWin += 30
-
-        # encourage more food gathering
-        if myFood < 1:
-            occupyWin += 20
-
-        # never want 0 workers
-        if len(myWorkers) < 1:
-            occupyWin += 100
-        if len(myWorkers) > 1:
-            occupyWin += 100
-
-        # want to kill enemy queen
-        if enemyQueen == None:
-            occupyWin -= 1000
-
-        # calculation for soldier going
-        # to kill enemyworker and after
-        # going to sit on enemy anthill
-        dist = 100
-        for ant in myOffense:
-            if len(enemyWorkers) == 0:
-                if not enemyQueen == None:
-                    dist = approxDist(ant.coords, enemyHill.coords)
-            else:
-                dist = approxDist(ant.coords, enemyWorkers[0].coords) + 10
-                if len(enemyWorkers) > 1:
-                    dist += 10
-
-        occupyWin += (dist) + (enemyHill.captureHealth)
-
-        # Gather food
-        foodWin = occupyWin
-        if not len(myOffense) > 0:
-            foodNeeded = 11 - myFood
-            for w in myWorkers:
-                distanceToTunnel = approxDist(w.coords, myTunnel.coords)
-                distanceToHill = approxDist(w.coords, myHill.coords)
-                distanceToFood = 9999
-                for food in foods:
-                    if approxDist(w.coords, food.coords) < distanceToFood:
-                        distanceToFood = approxDist(w.coords, food.coords)
-                if w.carrying: # if carrying go to hill/tunnel
-                    foodWin += min(distanceToHill, distanceToTunnel) - 9.5
-                    if w.coords == myHill.coords or w.coords == myTunnel.coords:
-                        foodWin += 1.5
-                    if not len(myOffense) == 0:
-                        foodWin -= 1
-                else: # if not carrying go to food
-                    if w.coords == foods[0].coords or w.coords == foods[1].coords:
-                        foodWin += 1.2
-                        break
-                    foodWin += distanceToFood/3 - 1.5
-                    if not len(myOffense) == 0:
-                        foodWin -= 1
-            occupyWin += foodWin * (foodNeeded)
-
-        # encourage queen killing or sitting on tunnel
-        if not enemyQueen == None:
-            if enemyQueen.coords == enemyHill.coords:
-                occupyWin += 20
-
-        return 1-math.exp(-0.001*(occupyWin))
+        self.current_game_states.append((currentState, self.previous_move))
+        self.previous_move = move_to_make
+        return move_to_make
     
     ##
     #getAttack
@@ -321,7 +235,7 @@ class AIPlayer(Player):
         self.current_game_states = []
 
         file_ptr = open("./dict_dump.txt", "wb")
-        pickle.dump(self.state_utility, file_ptr)
+        pickle.dump(self.state_action_utility, file_ptr)
         file_ptr.close()
         
 
